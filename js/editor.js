c154e03355525ccddfcaf7c302af28e4a535d4e1
Boby <zsb0213@gmail.com>
2015-01-23 00:05:06 +0800

    condense code ---- use innerHTML

--- js/editor.js
+++ js/editor.js
@@ -34,95 +34,43 @@ var words = {
 
 $(document).ready(function () {
 
-    // init
-    //var comma = document.createTextNode(", ");
-    //var semicolon = document.createTextNode("; ");
-    // There has a question: why just can use onece?
+    //
+    // init. Data translate to serface
+    //
 
     // init phonetic-symbol
     var phonBlock = document.getElementById("phonetic-symbol");
-    var addPs = document.getElementById("add-ps");
+    var phonBlockHTML = ""; // 
 
     for (var i = 0; i < words.phonetic.length; ++i) {
-        var newOnePs = document.createElement("div");
-        newOnePs.className = "one-ps";
 
-        var newDelete = document.createElement("img");
-        newDelete.className = "delete";
-        newDelete.src = "image/delete.png";
-        newOnePs.appendChild(newDelete);
+        var newPs = "<span class=\"ps\">" + words.phonetic[i] + "</span>";
+        var newDelete = "<img class=\"delete\" src=\"image/delete.png\" />";
+        var newOnePs = "<div class=\"one-ps\">" + newPs + newDelete + "</div>"
 
-        var newPs = document.createElement("span");
-        newPs.className = "ps";
-
-        var phon = document.createTextNode(words.phonetic[i]);
-        newPs.appendChild(phon);
-
-        newOnePs.insertBefore(newPs, newDelete);
-        phonBlock.insertBefore(newOnePs, addPs);
+        phonBlockHTML += newOnePs;
     }
-    
 
-    // delete phonetic-symbol
-    var phonBlock = document.getElementById("phonetic-symbol");
-    var deletePhon = phonBlock.getElementsByClassName("detele");
-
-    var deleteBlock = $(".delete");
-    deleteBlock.click(function () {
-        var onePs = this.parentNode;
-        phonBlock.removeChild(onePs);
-    })
-
-    // add new phonetic-symbol
-    var addPs = document.getElementById("add-ps");
-    addPs.onclick = function () {
-        var newOnePs = document.createElement("div");
-        newOnePs.className = "one-ps";
-
-        var newPs = document.createElement("span");
-        newPs.className = "ps";
-        newPs.contentEditable = "true";
-        var phon = document.createTextNode("/here/");
-        newPs.appendChild(phon);
-
-        var newDelete = document.createElement("img");
-        newDelete.className = "delete";
-        newDelete.src = "image/delete.png";
-
-        newOnePs.appendChild(newPs);
-        newOnePs.appendChild(newDelete);
-        phonBlock.insertBefore(newOnePs, addPs);
-
-        // bind clickEvent for new delete
-        newDelete.onclick = function () {
-            var onePs = this.parentNode;
-            phonBlock.removeChild(onePs);
-        }
-    }
+    phonBlockHTML += "<div id=\"add-ps\">添加发音</div>";
+    phonBlock.innerHTML = phonBlockHTML;
 
     // init bref-block
     var brefBlock = document.getElementById("bref-block");
+    var brefBlockHTML = "";
 
     for (var i = 0; i < 4; ++i) {
         if (words.characteristic[i].exist == true) {  // if this chara is exist
 
-            var brefLi = document.createElement("li");
-            brefLi.className = "characteristic";
-            brefLi.className += " cha-" + words.characteristic[i].chara;
+            // frist is characteristic's icon
+            var charaIcon = "<img class=\"ico-" + words.characteristic[i].chara
+                + "\" src=\"image/" + words.characteristic[i].chara + ".png\" />";
 
-            var img = document.createElement("img");
-            img.className = "ico-" + words.characteristic[i].chara;
-            img.src = "image/" + words.characteristic[i].chara + ".png";
-            brefLi.appendChild(img);
-
-            var translate = document.createElement("div");
-            translate.className = "translate";
-
+            // secend is bref translate block
             var brefString = "";
             var brefLength = words.characteristic[i].bref.length;
             for (var j = 0; j < brefLength; ++j) {
                 for (var k = 0; k < words.characteristic[i].bref[j].length; ++k) {
-                    
+
                     brefString += words.characteristic[i].bref[j][k];
 
                     if (k != words.characteristic[i].bref[j].length - 1) {
@@ -134,31 +82,29 @@ $(document).ready(function () {
                     brefString += "; ";
                 }
             }
-            translate.innerHTML = "<span class=\"c-tran\" contenteditable=\"true\">" +
+            var translateBlock = "<span class=\"translate\" contenteditable=\"true\">" +
                 brefString + "</span>";
-            brefLi.appendChild(translate);
-            brefBlock.appendChild(brefLi);
+
+            var brefLi = "<li class=\"characteristic cha-" + words.characteristic[i].chara + "\">"
+                + charaIcon + translateBlock + "</li>";
+
+            brefBlockHTML += brefLi;
         }
     }
+    brefBlock.innerHTML = brefBlockHTML;
 
     // init detail-block
     var detailBlock = document.getElementById("detail-block");
+    var detailBlockHTML = "";
 
     for (var i = 0; i < 4; ++i) {
         if (words.characteristic[i].exist == true) {  // if this chara is exist
 
-            var detailLi = document.createElement("li");
-            detailLi.className = "characteristic";
-            detailLi.className += " cha-" + words.characteristic[i].chara;
+            // frist is characteristic's icon
+            var charaIcon = "<img class=\"ico-" + words.characteristic[i].chara
+                + "\" src=\"image/" + words.characteristic[i].chara + ".png\" />";
 
-            var img = document.createElement("img");
-            img.className = "ico-" + words.characteristic[i].chara;
-            img.src = "image/" + words.characteristic[i].chara + ".png";
-            detailLi.appendChild(img);
-
-            var translate = document.createElement("div");
-            translate.className = "translate";
-
+            // secend is bref translate block
             var detailString = ""; // init
             var detailLength = words.characteristic[i].detail.length;
             for (var j = 0; j < detailLength; ++j) {
@@ -175,33 +121,65 @@ $(document).ready(function () {
                     detailString += "; ";
                 }
             }
-            translate.innerHTML = "<span class=\"c-tran\" contenteditable=\"true\">" +
+            var translateBlock = "<span class=\"translate\" contenteditable=\"true\">" +
                 detailString + "</span>";
 
-            // sentence
-            var chineseSenText = document.createTextNode(words.characteristic[i].chSentence);
-            var englishSenText = document.createTextNode(words.characteristic[i].enSentence);
+            // thrid is detail sentence(bref dosen't have sentence)
+            var chineseSenText = words.characteristic[i].chSentence;
+            var englishSenText = words.characteristic[i].enSentence;
 
-            var chineseSen = document.createElement("span");
-            chineseSen.className = "chinese-s" + " c-sen";
-            chineseSen.contentEditable = true;
-            chineseSen.appendChild(chineseSenText);
+            var chineseSen = "<span class=\"chinese-s\" contenteditable=\"true\">"
+                + chineseSenText + "</span>";
+            
+            var englishSen = "<span class=\"english-s\" contenteditable=\"true\">"
+                + englishSenText + "</span>";
 
-            var englishSen = document.createElement("span");
-            englishSen.className = "english-s" + " c-sen";
-            englishSen.contentEditable = true;
-            englishSen.appendChild(englishSenText);
+            var sentenceBlcok = "<div class=\"sentence\">" + chineseSen
+                + englishSen + "</div>";
 
-            var sentence = document.createElement("div");
-            sentence.className = "sentence";
-            sentence.appendChild(chineseSen);
-            sentence.appendChild(englishSen);
+            var detailLi = "<li class=\"characteristic cha-" + words.characteristic[i].chara + "\">"
+                + charaIcon + translateBlock + sentenceBlcok + "</li>";
 
-            detailLi.appendChild(translate);
-            detailLi.appendChild(sentence);
-            detailBlock.appendChild(detailLi);
+            detailBlockHTML += detailLi;
         }
     }
+    detailBlock.innerHTML = detailBlockHTML;
+    
+
+
+    //
+    // When operate in phonetic-symbol
+    //
+
+    // delete phonetic-symbol
+    var deletePhon = phonBlock.getElementsByClassName("detele");
+
+    var deleteBlock = $(".delete");
+    deleteBlock.click(function () {
+        var onePs = this.parentNode;
+        phonBlock.removeChild(onePs);
+    })
+
+    // add new phonetic-symbol
+    var addPs = document.getElementById("add-ps");
+    addPs.onclick = function () {
+        var newOnePs = document.createElement("div");
+        newOnePs.className = "one-ps";
+        
+        var newPs = "<span class=\"ps\" contenteditable=\"true\">/here/</span>";
+        var newDelete = "<img class=\"delete\" src=\"image/delete.png\" />";
+
+        newOnePs.innerHTML = newPs + newDelete;
+        phonBlock.insertBefore(newOnePs, addPs);
+
+        // bind clickEvent for new delete
+        newDelete.onclick = function () {
+            var onePs = this.parentNode;
+            phonBlock.removeChild(onePs);
+        }
+    }
+
+    
 
     // face to data
     var save = document.getElementById("save");
@@ -209,9 +187,9 @@ $(document).ready(function () {
         
         var phonetic = document.getElementsByClassName("ps");
         var brefTranslate = document.getElementById("bref-tran")
-            .getElementsByClassName("c-tran");
+            .getElementsByClassName("translate");
         var detailTranslate = document.getElementById("detial-tran")
-            .getElementsByClassName("c-tran");
+            .getElementsByClassName("translate");
         var chSentence = document.getElementsByClassName("chinese-s");
         var enSentence = document.getElementsByClassName("english-s");
         
